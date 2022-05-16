6da05450d223fa7b91553c1ac5683300deda47f6
fif <fif@cdi.ch>
2022-05-16 07:25:15 +0100

    tutorials done

--- MauiApp1/Platforms/iOS/AppDelegate.cs
+++ MauiApp1/Platforms/iOS/AppDelegate.cs
@@ -6,6 +6,7 @@ namespace MauiApp1;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
+    //https://xabaras.medium.com/sending-apple-push-notifications-from-a-net-core-application-5c006d894b54
     //https://www.youtube.com/watch?v=vvq0etotS8M&ab_channel=CodingFeats
     //protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     protected override MauiApp CreateMauiApp()
